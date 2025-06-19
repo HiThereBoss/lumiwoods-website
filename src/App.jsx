d6e5b67aa749749ff1ce2f3c5ec19beabd92aa88
Emre Bozkurt <emrebozkurt2006@gmail.com>
2025-06-19 00:01:16 -0400

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Preview from './components/Preview'
 
 function App() {
 
